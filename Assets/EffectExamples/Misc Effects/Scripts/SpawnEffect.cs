19934a478429760633818d0fbf994bba67d261f7
Nathaniel <nberger204@gmail.com>
2022-05-27 10:56:37 -0700

    slight fixes

--- Assets/EffectExamples/Misc Effects/Scripts/SpawnEffect.cs
+++ Assets/EffectExamples/Misc Effects/Scripts/SpawnEffect.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class SpawnEffect : MonoBehaviour {
 
-    public float spawnEffectTime = 2;
-    public float pause = 1;
+    public float spawnEffectTime = 3;
+    public float pause = -3;
     public AnimationCurve fadeIn;
 
     ParticleSystem ps;
@@ -26,21 +26,6 @@ public class SpawnEffect : MonoBehaviour {
         ps.Play();
 
     }
-	
-	void Update ()
-    {
-        if (timer < spawnEffectTime + pause)
-        {
-            timer += Time.deltaTime;
-        }
-        else
-        {
-            ps.Play();
-            timer = 0;
-        }
 
 
-        _renderer.material.SetFloat(shaderProperty, fadeIn.Evaluate( Mathf.InverseLerp(0, spawnEffectTime, timer)));
-        
-    }
 }
